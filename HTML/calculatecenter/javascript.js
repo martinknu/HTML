70ce23a4d4cec5a199fd68500b2114b94c5615c4
martinknu <martinknu@gmail.com>
2023-09-30 23:31:53 +0200

    fdhjoisdfho

--- HTML/calculatecenter/javascript.js
+++ HTML/calculatecenter/javascript.js
@@ -1,76 +1,22 @@
-const div_1 = document.getElementById('change_div');
-console.log(div_1);
-div_1.classList.add("highlight")
-
-
-// Function to execute when button "btn001" is clicked
-function myFunction() {
-  document.getElementById("demo_p_001").innerHTML = "YOU CLICKED ME!";
-}
-
-
-// Eventlistener for button "btn002" click
-document.getElementById("btn002").addEventListener("click", myEventListener001);
-
-function myEventListener001() {
-    document.getElementById("demo_p_002").innerHTML = "YOU CLICKED ME! and I have an event listener";   
-
-    // Output the width and height of the div center_in_navigation
-
-    const elmnt = document.getElementsByClassName("center_in_navigation")[0];
-    console.log(elmnt);
-    let text = "Height with padding and border: " + elmnt.offsetHeight + "px<br>";
-    text += "Width with padding and border: " + elmnt.offsetWidth + "px";
-    console.log(text);
-    document.getElementById("center_in_navigation_w").innerHTML = elmnt.offsetWidth;
-    document.getElementById("center_in_navigation_h").innerHTML = elmnt.offsetHeight;
-}
-
-
-var my_div = document.getElementById("center_in_navigation_w");
-var width = my_div.offsetWidth;
-
-if (window.addEventListener) {  // all browsers except IE before version 9
-  window.addEventListener ("resize", onResizeEvent, true);
-} else {
-  if (window.attachEvent) {   // IE before version 9
-    window.attachEvent("onresize", onResizeEvent);
-  }
-}
-
-function onResizeEvent() {
-  const elmnt = document.getElementsByClassName("center_in_navigation")[0];
-  let text = "Height with padding and border: " + elmnt.offsetHeight + "px<br>";
-  text += "Width with padding and border: " + elmnt.offsetWidth + "px";
-  document.getElementById("center_in_navigation_w").innerHTML = elmnt.offsetWidth;
-  document.getElementById("center_in_navigation_h").innerHTML = elmnt.offsetHeight;
-  console.log("mouseover");
-}
-
-
+// ON mouseover
 this_is_the_center.onmouseover = function(event){
 
   const elmnt = document.getElementsByClassName("center_in_navigation")[0];
-  let text = "Height with padding and border: " + elmnt.offsetHeight + "px<br>";
-  let clientWidth = elmnt.getBoundingClientRect();
-  text += "Width with padding and border: " + elmnt.offsetWidth + "px";
-  document.getElementById("center_in_navigation_w").innerHTML = elmnt.offsetWidth;
-  document.getElementById("center_in_navigation_h").innerHTML = clientWidth;
+  let elementWidth = elmnt.offsetWidth;
+  let boundingWidth = elmnt.getBoundingClientRect().width;
+  document.getElementById("center_in_navigation_w").innerHTML = "Width: " + elementWidth;
+  document.getElementById("center_in_navigation_wb").innerHTML = "bounding Width: " + boundingWidth;
   console.log("mouseover");
-  console.log(clientWidth);
+  console.log(elmnt.getBoundingClientRect());
 };
 
-
-
+// ON mouseout
 this_is_the_center.onmouseout = function(event){
 
   const elmnt = document.getElementsByClassName("center_in_navigation")[0];
-  let text = "Height with padding and border: " + elmnt.offsetHeight + "px<br>";
-  let clientWidth = elmnt.getBoundingClientRect();
-  text += "Width with padding and border: " + elmnt.offsetWidth + "px";
-  document.getElementById("center_in_navigation_w").innerHTML = elmnt.offsetWidth;
-  document.getElementById("center_in_navigation_h").innerHTML = clientWidth;
+  let elementWidth = elmnt.offsetWidth;
+  let boundingWidth = elmnt.getBoundingClientRect().width;
+  document.getElementById("center_in_navigation_w").innerHTML = "Width: " + elementWidth;
+  document.getElementById("center_in_navigation_wb").innerHTML = "bounding Width: " + boundingWidth;
   console.log("mouseout");
-  console.log(clientWidth);
 };
-//setInterval(onResizeEvent, 2000);
