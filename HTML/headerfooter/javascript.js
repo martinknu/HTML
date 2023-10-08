5c321b2b72e69edaf4cdf80264d69fa0373f4f08
martinknu <martinknu@gmail.com>
2023-10-08 22:04:56 +0200

    dss

---
+++ HTML/headerfooter/javascript.js
@@ -0,0 +1,14 @@
+
+
+// Get the template element
+
+let template = document.getElementById("header");
+
+// Clone the template content
+
+let clone = template.content.cloneNode(true);
+
+// Insert the clone into the document body
+
+document.body.appendChild(clone);
+//document.body.insertBefore()
