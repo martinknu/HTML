4b93ff49a164d2bd89ffabd99d22967a45d27df5
martinknu <martinknu@gmail.com>
2023-10-27 16:22:34 +0200

    jj

--- HTML/headerfooter/javascript.js
+++ HTML/headerfooter/javascript.js
@@ -1,14 +1,14 @@
 
+// Get the insertpoint
+let insertpoint = document.getElementById("pagecontainer");
 
 // Get the template element
-
 let template = document.getElementById("header");
 
 // Clone the template content
-
 let clone = template.content.cloneNode(true);
 
 // Insert the clone into the document body
 
-document.body.appendChild(clone);
-//document.body.insertBefore()
+//document.body.appendChild(clone);
+document.body.insertBefore(clone,insertpoint)
